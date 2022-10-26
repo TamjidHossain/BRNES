e1ca2b844a9364a11ad1c4b3efd55dc5a7ad4e5d
TamjidHossain <41366189+TamjidHossain@users.noreply.github.com>
2022-10-25 17:19:42 -0700

    Update RandomInit.py

--- Main/RandomInit.py
+++ Main/RandomInit.py
@@ -7,17 +7,18 @@ import pickle
 import sys
 
 noTarget = 1 # target will always be one
-gridHeightList = [sys.argv[1]]
-gridWidthList = [sys.argv[2]]
-noAgentList = [sys.argv[3]]
-noObsList = [sys.argv[4]]
-eList = [sys.argv[5]]
-LoopVal = sys.argv[6] # defines how many times the code will run
-neighborWeightsList = [sys.argv[7]]
+gridHeightList = [int(sys.argv[1])]
+gridWidthList = [int(sys.argv[2])]
+noAgentList = [int(sys.argv[3])]
+noObsList = [int(sys.argv[4])]
+eList = [int(sys.argv[5])]
+LoopVal = int(sys.argv[6]) # defines how many times the code will run
+neighborWeightsList = [float(sys.argv[7])]
 
 print("C"+str(len(gridWidthList))+ "_L"+str(LoopVal)+
               "_H"+str(gridHeightList[0])+"_W"+str(gridWidthList[0])+"_N"+str(noAgentList[0])+
-              "_O"+str(noObsList[0])+"_E"+str(eList[0])+"_Nw"+str(neighborWeightsList[0])
+              "_O"+str(noObsList[0])+"_E"+str(eList[0])+"_Nw"+str(neighborWeightsList[0]))
+
 
 # initializing the lists
 tPosList = []
@@ -68,11 +69,5 @@ for CriteriaVal in range(len(gridWidthList)):
               "_O"+str(Obs)+"_E"+str(epoch)+"_Nw"+str(neighborWeights), "wb") as Pp:   #Pickling
         pickle.dump(p, Pp)
         
-print("################# Random Initialization Done. Please find the pickle file inside RandomPosition folder #########################")
-
-
-
-
-
-
-
+print("################# Random Initialization Done. #########################")
+print("################# Please find the pickle file inside RandomPosition folder #########################")
