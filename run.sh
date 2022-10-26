664da24f90759c64b384832cbd57e65a0724c92d
TamjidHossain <41366189+TamjidHossain@users.noreply.github.com>
2022-10-25 19:52:36 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@
 # L: number of times the code will run as a loop, Nw: Neighbor weights [0,1], Ap: Attack Percentage [0,100]
 
 python RandomInit.py G N O E L Nw   # Example python RandomInit.py 5 5 1 1000 2 0.90
-python BRNES.py G N O E L Nw Ap   # Example python RandomInit.py 5 5 1 1000 2 0.90 30
+python BRNES.py G N O E L Nw Ap   # Example python BRNES.py 5 5 1 1000 2 0.90 30
