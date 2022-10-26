daee555e23f560c54acf1969b59cf44ebe3239ac
TamjidHossain <41366189+TamjidHossain@users.noreply.github.com>
2022-10-25 17:26:15 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python BRNETp.ipynb
+
+# G: Grid Height and Width (N x N), N: number of agents, O: number of obstacles, E: Total Episode,
+# L: number of times the code will run as a loop, Nw: Neighbor weights [0,1]
+
+python RandomInit.py G N O E L Nw   # Example python RandomInit.py 5 5 1 1000 2 0.90
